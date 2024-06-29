a69fc2134d1923338d0b8716b336aa0e300a7557
Bremackroy <bremackroy@gmail.com>
2024-06-29 16:07:13 -0400

    First Update

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,56 @@
+import {puppyList} from './data.js'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+  const [count, setCount] = useState(puppyList)
+  console.log(puppies)
+  return (
+    <>
+      <div>
+        {
+          puppies.map((puppy) => {
+            return <p>{puppy.name}</p>
+          })
+        }
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+       function App() {
+          const [puppies, setPuppies] = useState(puppyList);
+          console.log("puppyList: ", puppyList)
+
+          return (
+            <div className='App'>
+              {
+                puppies.map((puppy) => {
+                  return <p key={puppu.id}>{puppy.name}</p>
+                })
+              }
+            </div>
+          );
+        }
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
